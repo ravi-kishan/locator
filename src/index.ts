91cf8c3b50a82dc676634365f8f685d9789f056f
ravi <ravikishanaka@gmail.com>
2020-07-22 19:06:48 +0530

    fix: ts

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,49 @@
+
 var hash = "";
 var version = "1.0.0";
 var entryPoints = new Array();
+
 function setup() {
-    entryPoints.sort();
+    entryPoints.sort();    
 }
-export function register(entryPoint) {
+
+export function register(entryPoint:string) {
     entryPoints.push(entryPoint);
     hash = hash + '0';
     setup();
 }
+
 export function getHash() {
     return hash;
 }
+
 export function getVersion() {
     return version;
 }
-export function setVersion(versn) {
+
+export function setVersion(versn:string) {
     version = versn;
 }
-export function cached(entryPoint) {
-    entryPoints.forEach((value, index, array) => {
-        if (value == entryPoint) {
+
+export function cached(entryPoint:string) {
+    entryPoints.forEach((value,index,array) => {
+        if(value == entryPoint) {
             const cipherChars = [...hash]; // convert into array
             cipherChars[index] = '1'; // alter array
             hash = cipherChars.join(""); // convert back into string
+            console.log(hash);
         }
-    });
+    })
 }
-export function lazyCall(entryPoint) {
+
+export function lazyCall(entryPoint:string) {
     var found = 0;
-    entryPoints.forEach((value, index, array) => {
-        if (value == entryPoint) {
-            if (hash[index] == '1')
-                found = 1;
+    entryPoints.forEach((value,index,array) => {
+        if(value == entryPoint) {
+           if(hash[index] == '1')found = 1;
         }
-    });
-    if(found == 1)return "";
+    })
+    if( found == 1)return "";
     var script = entryPoint;
     script += "?";
     script += "v=" + version;
