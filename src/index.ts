d95763b7b80bfdc3c6e71c3e77c1ab1fe5694084
ravi <ravikishanaka@gmail.com>
2020-07-22 18:50:04 +0530

    fix: network call

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,41 @@
-
 var hash = "";
 var version = "1.0.0";
 var entryPoints = new Array();
-
 function setup() {
-    entryPoints.sort();    
+    entryPoints.sort();
 }
-
-export function register(entryPoint:string) {
+export function register(entryPoint) {
     entryPoints.push(entryPoint);
     hash = hash + '0';
     setup();
 }
-
 export function getHash() {
     return hash;
 }
-
 export function getVersion() {
     return version;
 }
-
-export function setVersion(versn:string) {
+export function setVersion(versn) {
     version = versn;
 }
-
-export function cached(entryPoint:string) {
-    entryPoints.forEach((value,index,array) => {
-        if(value == entryPoint) {
+export function cached(entryPoint) {
+    entryPoints.forEach((value, index, array) => {
+        if (value == entryPoint) {
             const cipherChars = [...hash]; // convert into array
             cipherChars[index] = '1'; // alter array
             hash = cipherChars.join(""); // convert back into string
-            console.log(hash);
         }
-    })
+    });
 }
-
-export function lazyCall(entryPoint:string) {
-    entryPoints.forEach((value,index,array) => {
-        if(value == entryPoint) {
-           if(hash[index] == '1')return "";
+export function lazyCall(entryPoint) {
+    var found = 0;
+    entryPoints.forEach((value, index, array) => {
+        if (value == entryPoint) {
+            if (hash[index] == '1')
+                found = 1;
         }
-    })
+    });
+    if(found == 1)return "";
     var script = entryPoint;
     script += "?";
     script += "v=" + version;
