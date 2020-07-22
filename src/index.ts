3e52bceffa1d04333c6c33bcf44a20d9e99cc3f2
ravi <ravikishanaka@gmail.com>
2020-07-22 17:06:01 +0530

    feat: console

--- src/index.ts
+++ src/index.ts
@@ -31,6 +31,7 @@ export function cached(entryPoint:string) {
             const cipherChars = [...hash]; // convert into array
             cipherChars[index] = '1'; // alter array
             hash = cipherChars.join(); // convert back into string
+            console.log(hash);
         }
     })
 }
